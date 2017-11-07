99327fdce8e9edbd1e80fba58befc1eeb9293198
Tomhug83 <tomhug83jr@gmail.com>
2017-11-06 23:00:25 -0500

    typo

--- startup.py
+++ startup.py
@@ -119,6 +119,6 @@ class startup():
 
 
 #bot.loop.create_task(get_uptime())
-with open("token.pk1", "r") as token_file:
+with open("token_file.pk1", "r") as token_file:
   token = json.load(token_file)
   bot.run(token)
