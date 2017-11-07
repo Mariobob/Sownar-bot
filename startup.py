c50af8c103131f6617d764de152aa119d8080c34
Tomhug83 <tomhug83jr@gmail.com>
2017-11-06 22:16:25 -0500

    u

--- startup.py
+++ startup.py
@@ -24,95 +24,98 @@ game = ('{0}help | {1} servers'.format(prefix, len(bot.servers)))
 startup_extensions = ["utils", "mod", "fun", "owner", "random", "main"]
 perm_error = discord.Embed(title=":warning: Error!",description="You do not have the permission to use this command",color=0xff0000)
 
-@bot.event()
-async def on_ready():
-  print("Logged in as")
-  print(bot.user.name)
-  print(bot.user.id)
-  print("------")
-  bot.load_extension("utils")
-  bot.load_extension("fun")
-  bot.load_extension("main")
-  bot.load_extension("owner")
-  bot.load_extension("random")
-  bot.load_extension("mod")
+class startup():
+  
+  @bot.event()
+  async def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("------")
+    bot.load_extension("utils")
+    bot.load_extension("fun")
+    bot.load_extension("main")
+    bot.load_extension("owner")
+    bot.load_extension("random")
+    bot.load_extension("mod")
 
-@bot.event()
-async def get_uptime():
-    await bot.wait_until_ready()
-    global seconds
-    seconds = 0
-    global minutes
-    minutes = 0
-    global hours
-    hours = 0
-    global days
-    days = 0
-    global weeks
-    weeks = 0
-    while not bot.is_closed:
-        await asyncio.sleep(1)
-        seconds += 1
-        if seconds==60:
-            minutes += 1
-            seconds = 0
-        if minutes==60:
-            hours += 1
-            minutes = 0
-        if hours==24:
-            days += 1
-            hours = 0
-        if days==7:
-            weeks += 1
-            days = 0
-
-@bot.command()
-async def uptime():
+  @bot.event()
+  async def get_uptime():
+      await bot.wait_until_ready()
+      global seconds
+      seconds = 0
+      global minutes
+      minutes = 0
+      global hours
+      hours = 0
+      global days
+      days = 0
+      global weeks
+      weeks = 0
+      while not bot.is_closed:
+          await asyncio.sleep(1)
+          seconds += 1
+          if seconds==60:
+              minutes += 1
+              seconds = 0
+          if minutes==60:
+              hours += 1
+              minutes = 0
+          if hours==24:
+              days += 1
+              hours = 0
+          if days==7:
+              weeks += 1
+              days = 0
+  
+  @bot.command()
+  async def uptime():
     msg = "{0} weeks, {1} days, {2} hours, {3} minutes and {4} seconds".format(weeks, days, hours, minutes, seconds)
     embed = discord.Embed(title="__Bot Uptime__", description=msg, color=0x000000)
     await bot.say(embed=embed)
 
-@bot.command(pass_context = True)
-async def reload(ctx, ext_name: str):
-    if ctx.message.author.id not in ownerids:
-        await bot.say(embed=perm_error)
-    else:
-      if ext_name in startup_extensions:
-        bot.unload_extension(ext_name)
-        bot.load_extension(ext_name)
-        await bot.say("Extension **{}** reloaded".format(ext_name))
+  @bot.command(pass_context = True)
+  async def reload(ctx, ext_name: str):
+      if ctx.message.author.id not in ownerids:
+          await bot.say(embed=perm_error)
       else:
-        await bot.say("Not a valid extension, see s.extlist for options")
+        # TODO: write code...se:
+        if ext_name in startup_extensions:
+          bot.unload_extension(ext_name)
+          bot.load_extension(ext_name)
+          await bot.say("Extension **{}** reloaded".format(ext_name))
+        else:
+          await bot.say("Not a valid extension, see s.extlist for options")
         
-@bot.command(pass_context = True)
-async def load(ctx, ext_name: str):
-    if ctx.message.author.id not in ownerids:
-        await bot.say(embed=perm_error)
-    else:
-      if ext_name in startup_extensions:
-        bot.load_extension(ext_name)
-        await bot.say("Extension **{}** loaded".format(ext_name))
+  @bot.command(pass_context = True)
+  async def load(ctx, ext_name: str):
+      if ctx.message.author.id not in ownerids:
+          await bot.say(embed=perm_error)
       else:
-        await bot.say("Not a valid extension, see s.extlist for options")
+        if ext_name in startup_extensions:
+          bot.load_extension(ext_name)
+          await bot.say("Extension **{}** loaded".format(ext_name))
+        else:
+          await bot.say("Not a valid extension, see s.extlist for options")
     
-@bot.command(pass_context = True)
-async def unload(ctx, ext_name: str):
-    if ctx.message.author.id not in ownerids:
-        await bot.say(embed=perm_error)
-    else:
-      if ext_name in startup_extensions:
-        bot.unload_extension(ext_name)
-        await bot.say("Extension **{}** unloaded".format(ext_name))
+  @bot.command(pass_context = True)
+  async def unload(ctx, ext_name: str):
+      if ctx.message.author.id not in ownerids:
+          await bot.say(embed=perm_error)
       else:
-        await bot.say("Not a valid extension, see s.extlist for options")
+        if ext_name in startup_extensions:
+          bot.unload_extension(ext_name)
+          await bot.say("Extension **{}** unloaded".format(ext_name))
+        else:
+          await bot.say("Not a valid extension, see s.extlist for options")
         
-@bot.command(pass_context = True)
-async def extlist(ctx):
-    if ctx.message.author.id not in ownerids:
-        await bot.say(embed=perm_error)
-    else:
-        for item in todo_list:
-            await bot.say('`item`')
+  @bot.command(pass_context = True)
+  async def extlist(ctx):
+      if ctx.message.author.id not in ownerids:
+          await bot.say(embed=perm_error)
+      else:
+          for item in todo_list:
+              await bot.say('`item`')
 
 
 bot.loop.create_task(get_uptime())
