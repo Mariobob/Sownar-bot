b02844649137cbeaa672b4da16d26126476c3422
Tomhug83 <tomhug83jr@gmail.com>
2017-11-06 22:21:59 -0500

    indent error fix

--- startup.py
+++ startup.py
@@ -40,32 +40,32 @@ class startup():
     bot.load_extension("mod")
 
   async def get_uptime():
-      await bot.wait_until_ready()
-      global seconds
-      seconds = 0
-      global minutes
-      minutes = 0
-      global hours
-      hours = 0
-      global days
-      days = 0
-      global weeks
-      weeks = 0
-      while not bot.is_closed:
-          await asyncio.sleep(1)
-          seconds += 1
-          if seconds==60:
-              minutes += 1
-              seconds = 0
-          if minutes==60:
-              hours += 1
-              minutes = 0
-          if hours==24:
-              days += 1
-              hours = 0
-          if days==7:
-              weeks += 1
-              days = 0
+    await bot.wait_until_ready()
+    global seconds
+    seconds = 0
+    global minutes
+    minutes = 0
+    global hours
+    hours = 0
+    global days
+    days = 0
+    global weeks
+    weeks = 0
+    while not bot.is_closed:
+        await asyncio.sleep(1)
+        seconds += 1
+        if seconds==60:
+            minutes += 1
+            seconds = 0
+        if minutes==60:
+            hours += 1
+            minutes = 0
+        if hours==24:
+            days += 1
+            hours = 0
+        if days==7:
+            weeks += 1
+            days = 0
   
   @bot.command()
   async def uptime():
