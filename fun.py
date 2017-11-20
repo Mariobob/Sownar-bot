55b9076ff317d94d42500e3157447e29d8798286
Tomhug83 <tomhug83jr@gmail.com>
2017-11-19 22:57:46 -0500

    added 3 commands

--- fun.py
+++ fun.py
@@ -19,6 +19,45 @@ class Fun():
     @bot.command(pass_context = True)
     async def fun(ctx):
         await ctx.bot.say("I am working!")
+      
+    @bot.command(pass_context = True)
+    async def flip(ctx):
+      flip = random.choice["Heads", "Tails"]
+      await ctx.bot.say("It's {}!".format(flip))
+    
+    @bot.command(pass_context = True)
+    async def roll(ctx):
+      roll = random.choice["1", "2", "3", "4", "5", "6"]
+      await ctx.bot.say("It's a {}!".format(roll))
+      
+    @bot.command(pass_context = True)
+    async def rps(ctx, rps = str):
+      if rps == "":
+        await ctx.bot.say("Please specify [rock, paper or scissors]")
+      else:
+        rps_bot = random.choice["Rock", "Paper", "Scissors"]
+        if rps == "rock":
+          if rps_bot == "Paper":
+            await ctx.bot.say("Paper! I win")
+          elif rps_bot == "Scissors":
+            await ctx.bot.say("Scissors! You win")
+          elif rps_bot == "Rock":
+            await ctx.bot.say("Rock! It's a tie")
+        elif rps == "paper":
+          if rps_bot == "Rock":
+            await ctx.bot.say("Rock! You win")
+          elif rps_bot == "Scissors":
+            await ctx.bot.say("Scissors! I win")
+          elif rps_bot == "Paper":
+            await ctx.bot.say("Paper! It's a tie")
+        elif rps == "scissors":
+          if rps_bot == "Rock":
+            await ctx.bot.say("Rock! I win")
+          elif rps_bot == "Paper":
+            await ctx.bot.say("Paper! You win")
+          elif rps_bot == "Scissors":
+            await ctx.bot.say("Scissors! It's a tie")
+          
 
 def setup(bot):
     bot.add_cog(Fun)
