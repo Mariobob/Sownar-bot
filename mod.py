689c0a0bf5a9cd4168e5c3e173777d39ef623596
Tomhug83 <tomhug83jr@gmail.com>
2017-11-30 18:27:48 -0500

    fixed bug

--- mod.py
+++ mod.py
@@ -24,36 +24,36 @@ class Mod():
     @bot.command(pass_context = True)
     async def ban(ctx, *, member: discord.Member = None):
         if member is None:
-            await bot.say("Who do I ban ?")
+            await ctx.bot.say(ctx.message.author + ": Who do I ban ?")
     
         elif ctx.message.server.me.server_permissions.ban_members == True:
                 if ctx.message.author.server_permissions.ban_members == True:
                     if ctx.message.author.top_role.position > member.top_role.position:
-                        await bot.ban(member)
-                        await bot.say(":white_check_mark: Succesfully banned {}".format(member))
+                        await ctx.bot.ban(member)
+                        await ctx.bot.say(":white_check_mark: Succesfully banned {}".format(member))
                     else:
-                        await bot.say(embed=perm_error)
+                        await ctx.bot.say(embed=perm_error)
                 else:
-                    await bot.say(embed=perm_error)
+                    await ctx.bot.say(embed=perm_error)
         else:
-            await bot.say(embed=perm_errorbis)
+            await ctx.bot.say(embed=perm_errorbis)
             
     @bot.command(pass_context = True)
     async def kick(ctx, *, member: discord.Member = None):
         if member is None:
-            await bot.say("Who do I kick ?")
+            await ctx.bot.say(ctx.message.author + ": Who do I kick ?")
     
         if ctx.message.server.me.server_permissions.kick_members == True:
                 if ctx.message.author.server_permissions.kick_members == True:
                     if ctx.message.author.top_role.position > member.top_role.position:
-                        await bot.kick(member)
-                        await bot.say(":white_check_mark: Succesfully kicked {}".format(member))
+                        await ctx.bot.kick(member)
+                        await ctx.bot.say(":white_check_mark: Succesfully kicked {}".format(member))
                     else:
-                        await bot.say(embed=perm_error)
+                        await ctx.bot.say(embed=perm_error)
                 else:
-                    await bot.say(embed=perm_error)
+                    await ctx.bot.say(embed=perm_error)
         else:
-            await bot.say(embed=perm_errorbis)
+            await ctx.bot.say(embed=perm_errorbis)
     
     
 
