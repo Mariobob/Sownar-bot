be85908ba56b5e1ba8e1bfda4d7bfa9c01542ecf
Tomhug83 <tomhug83jr@gmail.com>
2017-12-07 22:39:11 -0500

    u

--- cool.py
+++ cool.py
@@ -7,6 +7,7 @@ import datetime
 from discord.ext import commands
 import time
 import traceback
+import aiohttp
 
 
 prefix='s.'
@@ -27,8 +28,35 @@ class Cool():
       await ctx.bot.say(echo)
     
     @bot.command(pass_context = True)
-    async def dog(ctx):
-      await ctx.bot.say(https://random.dog/)
+    async def cat(ctx):
+      api = 'https://random.cat/meow'
+      async with aiohttp.ClientSession() as session:
+          async with session.get(api) as r:
+              if r.status == 200:
+                  response = await r.json()
+                  embed = discord.Embed(title="Random Cat picture!", description="")
+                  embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
+                  embed.set_image(url=response['file'])
+                  await ctx.bot.sat(embed=embed)
+              else:
+                  await ctx.bot.say('Error accessing the API')
+    
+    @bot.command(pass_context = True)
+    async def dog(ctx):            
+      api = "https://api.thedogapi.co.uk/v2/dog.php"
+      async with aiohttp.ClientSession() as session:
+          async with session.get(api) as r:
+              if r.status == 200:
+                  response = await r.json()
+                  embed = discord.Embed(title="Random Dog picture!", description="")
+                  embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
+                  embed.set_image(url=response['data'][0]["url"])
+                  await ctx.bot.say(embed=embed)
+              else:
+                  x = "Could not find a dog :sad:!"
+                  embed = discord.Embed(title='Error')
+                  embed.description = x
+                  await ctx.bot.say(embed=embed)
 
 def setup(bot):
     bot.add_cog(Cool)
