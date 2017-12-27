fc3a5902ebfaf79ba0f0146ae016154e0b96eb80
Tomhug83 <tomhug83jr@gmail.com>
2017-12-27 14:42:31 -0500

    u

--- fun.py
+++ fun.py
@@ -179,29 +179,45 @@ class Fun():
     @casino.command(pass_context=True, aliases=["21", "bj"])
     async def blackjack(ctx):
       bj_continue = 1
+      player=randint(1,13)
+      dealer=randint(1,13)
+      dealshow="N/A"
+      done = "false"
+      
       def bj_embed():
-        bj = discord.Embed(title="---Hand---", description="`Player: {0}`\n`Dealer: {1}`".format(player, dealshow))
-        bj.set_author(name="BlackJack", icon_url="http://www.emoji.co.uk/files/twitter-emojis/symbols-twitter/11272-playing-card-black-joker.png")
-        bj.add_field(name="`Stand or Hit`?", value="Please type `stop` to take no more cards\nPlease type `continue` to take more cards", inline = False)
-        bj.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
-        return bj
-      def bj_message_checker(text):
-        if text == "stop":
-          dealshow = dealer
-          bj_continue = 0
-          return True
-        elif text == "continue":
-          dealer += randint(1,13)
-          player += randint(1,13)
-          bj_continue = 1 
-          return True
+        if done == "false":
+          bj = discord.Embed(title="---Hand---", description="`Player: {0}`\n`Dealer: {1}`".format(player, dealshow))
+          bj.set_author(name="BlackJack", icon_url="http://www.emoji.co.uk/files/twitter-emojis/symbols-twitter/11272-playing-card-black-joker.png")
+          bj.add_field(name="`Stand or Hit`?", value="Please type `stop` to take no more cards\nPlease type `continue` to take more cards", inline = False)
+          bj.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+          return bj
+        elif done == "true":
+          bj = discord.Embed(title="---Hand---", description="`Player: {0}`\n`Dealer: {1}`".format(player, dealshow))
+          bj.set_author(name="BlackJack", icon_url="http://www.emoji.co.uk/files/twitter-emojis/symbols-twitter/11272-playing-card-black-joker.png")
+          bj.add_field(name="You {}".format(winorlose), value = "-", inline = False)
+          bj.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+          return bj
+        
+      def ctx.bot.on_reaction_add(react, user):
+        if user == ctx.message.author:
+          if react == "2705":
+            player += randint(1,13)
+            dealer += randint(1,13)
+            if player > 21:
+              done = "true"
+            elif dealer > 21:
+              done = "true"
+            done = "false"
+          elif react == "274c":
+            dealshow = dealer
+            done = "true"
+            
+            
+        
       
       while bj_continue == 1:
-        player=randint(1,13)
-        dealer=randint(1,13)
-        dealshow="N/A"
-        await ctx.bot.say(embed=bj_embed())
-        msg = await bot.wait_for_message(author=ctx.message.author, check=bj_message_checker(msg))
+        bj_message = await ctx.bot.send_message(embed=bj_embed())
+        
       
       
       
