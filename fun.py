c71c2eb38b6a33df9cf214667e3d01ae8399ae38
Tomhug83 <tomhug83jr@gmail.com>
2017-12-26 21:11:43 -0500

    u

--- fun.py
+++ fun.py
@@ -139,9 +139,8 @@ class Fun():
       elif winner == "tied":
         dcolor = 0xffae00
         
-      war = discord.Embed(title = "-", description = "-=-", color=dcolor)
+      war = discord.Embed(title = "**---Cards---**", description = "`Player: {}` \n`Computer: {}`".format(player, ai), color=dcolor)
       war.set_author(name="War", icon_url="http://www.clipartlord.com/wp-content/uploads/2014/08/playing-card5.png")
-      war.add_field(name = "**---Cards---**", value="`Player: {}` \n`Computer: {}`".format(player, ai), inline = False)
       war.add_field(name = "You {0}".format(winner), value="-", inline = False)
       war.set_footer(icon_url= ctx.message.author.avatar_url, text= "Requested by {}".format(ctx.message.author.name))
       await ctx.bot.say(embed=war)
