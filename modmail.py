c17fe2eeca51d2a1d4c20f0b74da1281cc6dcfdc
Tomhug83 <tomhug83jr@gmail.com>
2017-12-29 20:24:43 -0500

    u

--- modmail.py
+++ modmail.py
@@ -13,7 +13,8 @@ bot=commands.Bot(command_prefix=prefix)
 ownerids=['221381001476046849', '221263215496134656']
 mm_error = discord.Embed(title=":warning: Error!",description="Please use this command in DM with the bot :wink:",color=0xff0000)
 perm_error = discord.Embed(title=":warning: Error!",description="You do not have the permission to use this command",color=0xff0000)
-
+support="https://discord.gg/HcMhj3q"
+bl_ids=[]
 
 class ModMail():
     print('ModMail loaded')
@@ -23,21 +24,36 @@ class ModMail():
     async def mm(ctx, *, msg:str):
       if ctx.message.channel.is_private is True:
         chan_id = ctx.message.channel.id
-        server= ctx.bot.get_server("396469778430296068")
-        chan= await ctx.bot.create_channel(server, chan_id)
-        embed= discord.Embed(title="ModMail with {}".format(ctx.message.author), description=msg)
-        await ctx.bot.send_message(chan, embed=embed)
+        if chan_id not in bl_ids:
+          server= ctx.bot.get_server("396469778430296068")
+          chan= await ctx.bot.create_channel(server, chan_id)
+          embed= discord.Embed(title="ModMail with {}".format(ctx.message.author), description=msg)
+          await ctx.bot.send_message(chan, embed=embed)
+        else:
+          mm_bl = discord.Embed(title=":warning: Error!",description="You have been blacklisted, please join the ({})[{}] if you consider this to be wrong".format("Support Server", support),color=0xff0000)
+          await ctx.bot.say(embed=mm_bl)
       else:
         await ctx.bot.say(embed=mm_error)
     
-    @bot.command(pass_context = True)
+    @bot.command(pass_context = True, no_pm = True)
     async def mma(ctx, *, msg:str):
       if ctx.message.author.id not in ownerids:
         await ctx.bot.say(embed=perm_error)
       else:
         user = discord.Object(ctx.message.channel.name)
-        mma = discord.Embed(title="A Dev answered your question: {}".format(ctx.message.author.name), description=msg)
+        mma = discord.Embed(title="A Dev answered your question: ({})".format(ctx.message.author.name), description=msg)
         await ctx.bot.send_message(user, embed=mma)
+        
+    @bot.command(pass_context = True, np_pm= True)
+    async def mmbl(ctx):
+      if ctx.message.author.id not in ownerids:
+        await ctx.bot.say(embed=perm_error)
+      else:
+        if ctx.message.channel.name in bl_ids:
+          bl_ids.remove(ctx.message.channel.name)
+        else:
+          bl_ids.append(ctx.message.channel.name)
+          
       
 
 
