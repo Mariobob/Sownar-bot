da0720db9dcdbf532ef167dd1c45a959efa239fa
Tomhug83 <tomhug83jr@gmail.com>
2017-12-29 19:59:28 -0500

    u

--- modmail.py
+++ modmail.py
@@ -10,12 +10,39 @@ import traceback
 
 prefix='s.'
 bot=commands.Bot(command_prefix=prefix)
+server= discord.Object("376096854448013325")
+ownerids=['221381001476046849', '221263215496134656']
+mm_error = discord.Embed(title=":warning: Error!",description="Please use this command in DM with the bot :wink:",color=0xff0000)
+perm_error = discord.Embed(title=":warning: Error!",description="You do not have the permission to use this command",color=0xff0000)
 
 
 class ModMail():
     print('ModMail loaded')
     print('------')
-
+  
+    @bot.command(pass_context = True, aliases=["modmail", "mailmod", "mail"])
+    async def mm(ctx, *, msg:str):
+      if ctx.message.channel.is_private is True:
+        chan_id = ctx.message.channel.id
+        everyone_perms = discord.PermissionOverwrite(read_messages=False)
+        my_perms = discord.PermissionOverwrite(read_messages=True)
+        everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
+        mine = discord.ChannelPermissions(target=server.me, overwrite=my_perms)
+        await ctx.bot.create_channel(server, 'chan_id', everyone, mine)
+        embed= discord.Embed(title="ModMail with {}".format(ctx.message.author), description=msg)
+        await ctx.bot.say(embed=embed)
+      else:
+        await ctx.bot.say(embed=mm_error)
+    
+    @bot.command(pass_context = True)
+    async def mma(ctx, *, msg:str):
+      if ctx.message.author.id not in ownerids:
+        await ctx.bot.say(embed=perm_error)
+      else:
+        user = discord.Object(ctx.message.channel.name)
+        mma = discord.Embed(title="A Dev answered your question: {}".format(ctx.message.author.name), description=msg)
+        await ctx.bot.send_message(user, embed=mma)
+      
 
 
 
