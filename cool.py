8ecd43e143c5e21de7a89cfc497e075184459437
Tomhug83 <tomhug83jr@gmail.com>
2017-12-31 01:27:33 -0500

    u

--- cool.py
+++ cool.py
@@ -88,31 +88,32 @@ class Cool():
     async def giveaway(ctx, time, title):
       try:
         time = int(time)
+        ga_users=[]
+        ga=discord.Embed(title=":tada: NEW GIVEAWAY :tada:", description="-")
+        ga.add_field(name=title, value="Ends in **{}** seconds".format(time), inline =False)
+        ga.set_footer(text="1 winner")
+        ga_react = await ctx.bot.say(embed=ga)
+        await ctx.bot.add_reaction(ga_react, "🎉")
+        await asyncio.sleep(time)
+        ga_message_id = ga_react.id
+        ga_channel = ga_react.channel
+        ga_message = await ctx.bot.get_message(ga_channel, ga_message_id)
+        for user in await ctx.bot.get_reaction_users(ga_message.reactions[0]):
+          ga_users.append(user.mention)
+        sownar = ctx.message.server.get_member("375370278810681344")
+        ga_users.remove(sownar.mention)
+        if len(ga_users) == 0:
+          error = discord.Embed(title=":warning: Error!",description="The giveaway ended with no participants, could not chose a winner",color=0xff0000)
+          await ctx.bot.say(embed=error)
+        else:
+          winner = random.choice(ga_users)
+        ga_end = discord.Embed(title=":tada: GIVEAWAY ENDED :tada:", description="Winner is {}".format(winner))
+        await ctx.bot.edit_message(ga_react, embed = ga_end)
+        await ctx.bot.say("Congrats {0}! You won **{1}**".format(winner, title))
       except:
         error = discord.Embed(title=":warning: Error!",description="Please use a time in seconds",color=0xff0000)
         await ctx.bot.say(embed=error)
-      ga_users=[]
-      ga=discord.Embed(title=":tada: NEW GIVEAWAY :tada:", description="-")
-      ga.add_field(name=title, value="Ends in **{}** seconds".format(time), inline =False)
-      ga.set_footer(text="1 winner")
-      ga_react = await ctx.bot.say(embed=ga)
-      await ctx.bot.add_reaction(ga_react, "🎉")
-      await asyncio.sleep(time)
-      ga_message_id = ga_react.id
-      ga_channel = ga_react.channel
-      ga_message = await ctx.bot.get_message(ga_channel, ga_message_id)
-      for user in await ctx.bot.get_reaction_users(ga_message.reactions[0]):
-        ga_users.append(user.mention)
-      sownar = ctx.message.server.get_member("375370278810681344")
-      ga_users.remove(sownar.mention)
-      if len(ga_users) == 0:
-        error = discord.Embed(title=":warning: Error!",description="The giveaway ended with no participants, could not chose a winner",color=0xff0000)
-        await ctx.bot.say(embed=error)
-      else:
-        winner = random.choice(ga_users)
-      ga_end = discord.Embed(title=":tada: GIVEAWAY ENDED :tada:", description="Winner is {}".format(winner))
-      await ctx.bot.edit_message(ga_react, embed = ga_end)
-      await ctx.bot.say("Congrats {0}! You won **{1}**".format(winner, title))
+      
       
     @bot.command(pass_context = True, no_pm = True, aliases = ["countdown"])
     async def cdown(ctx, time:int):
