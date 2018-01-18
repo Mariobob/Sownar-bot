11eb9207124a3a918c764087cd20141964447d1f
Tomhug83 <tomhug83jr@gmail.com>
2018-01-17 21:34:47 -0500

    u

--- dblAPI.py
+++ dblAPI.py
@@ -3,10 +3,13 @@ import aiohttp
 import asyncio
 import os
 import requests
+import discord
+from discord.ext import commands
 
 with open("token_file2.pk1", "r") as token_file2:
   dbl = json.load(token_file2)
 bot=commands.Bot(command_prefix='s.')
+ownerids=['221381001476046849', '221263215496134656']
 
 url = "https://discordbots.org/api/bots/" + '375370278810681344' + "/stats"
 headers = {"Authorization" : dbl}
@@ -28,6 +31,20 @@ class botsorgapi():
   async def on_server_remove(server):
     payload = {"server_count"  : len(bot.servers)}
     requests.post(url, data=payload, headers=headers)
+    
+  @bot.command(pass_context = True, hidden = True)
+  async def dblAPIload(ctx):
+    if ctx.message.author.id not in ownerids:
+      pass
+    else:
+      try:
+        payload = {"server_count"  : len(bot.servers)}
+        requests.post(url, data=payload, headers=headers)
+        await ctx.bot.say(":white_check_mark: Succes!")
+      except Exception as e:
+              embed = discord.Embed(title=":warning: Error!", description="Failed loading {0}\n{1}: {2}".format(extension, type(e).__name__, e), color=0xff0000)
+              await ctx.bot.say(embed=embed)
+      
 
 def setup(bot):
     bot.add_cog(botsorgapi)
