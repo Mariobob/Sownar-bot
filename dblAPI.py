f28d89d18505261908a89bfc54f9f3f567955270
Tomhug83 <tomhug83jr@gmail.com>
2018-01-17 21:30:29 -0500

    u

--- dblAPI.py
+++ dblAPI.py
@@ -6,6 +6,7 @@ import requests
 
 with open("token_file2.pk1", "r") as token_file2:
   dbl = json.load(token_file2)
+bot=commands.Bot(command_prefix='s.')
 
 url = "https://discordbots.org/api/bots/" + '375370278810681344' + "/stats"
 headers = {"Authorization" : dbl}
