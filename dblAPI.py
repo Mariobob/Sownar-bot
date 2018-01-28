647bf52badb5a71b2d78c7725859289f0a7c2f19
Tomhug83 <tomhug83jr@gmail.com>
2018-01-28 10:29:26 -0500

    u

--- dblAPI.py
+++ dblAPI.py
@@ -10,6 +10,18 @@ with open("token_file2.pk1", "r") as token_file2:
   dbl = json.load(token_file2)
 bot=commands.Bot(command_prefix='s.')
 ownerids=['221381001476046849', '221263215496134656']
+yup = discord.Object('376552211817299968')
+
+def apiload():
+  try:
+    payload = {"server_count"  : len(bot.servers)}
+    requests.post(url, data=payload, headers=headers)
+    succes = ':white_check_mark: Succes!'
+  except Exception as e:
+    succes = "Failed loading {0}\n{1}: {2}".format('API', type(e).__name__, e)
+  return succes
+    
+
 
 url = "https://discordbots.org/api/bots/" + '375370278810681344' + "/stats"
 headers = {"Authorization" : dbl}
@@ -19,31 +31,23 @@ class botsorgapi():
   
   @bot.event
   async def on_ready():
-    payload = {"server_count"  : len(bot.servers)}
-    requests.post(url, data=payload, headers=headers)
+    await bot.send_message(yup, apiload())
     
   @bot.event
   async def on_server_join(server):
-    payload = {"server_count"  : len(bot.servers)}
-    requests.post(url, data=payload, headers=headers)
+    await bot.send_message(yup, apiload())
     
   @bot.event
   async def on_server_remove(server):
-    payload = {"server_count"  : len(bot.servers)}
-    requests.post(url, data=payload, headers=headers)
+    await bot.send_message(yup, apiload())
+    
     
   @bot.command(pass_context = True, hidden = True)
   async def dblAPIload(ctx):
     if ctx.message.author.id not in ownerids:
       pass
     else:
-      try:
-        payload = {"server_count"  : len(ctx.bot.servers)}
-        requests.post(url, data=payload, headers=headers)
-        await ctx.bot.say(":white_check_mark: Succes!")
-      except Exception as e:
-              embed = discord.Embed(title=":warning: Error!", description="Failed loading {0}\n{1}: {2}".format(extension, type(e).__name__, e), color=0xff0000)
-              await ctx.bot.say(embed=embed)
+      await bot.say(apiload())
       
 
 def setup(bot):
